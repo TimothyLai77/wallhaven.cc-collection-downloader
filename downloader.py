714ec738043037f2a12847761c4354708b69c007
timothy <lai.timothy77@gmail.com>
2025-11-21 16:41:40 -0500

    downloads into subdirectory

--- downloader.py
+++ downloader.py
@@ -3,12 +3,16 @@ import requests
 import os
 import time
 # pass in API Key, and the collection you want to download
+
+
+
+
 def main():
     username = sys.argv[1]
     apiKey = sys.argv[2]
     collectionID = sys.argv[3]
     collection = getCollection(username, apiKey, collectionID);    
-    downloadWallpapers(collection)
+    downloadWallpapers(collection, collectionID)
 
     print('Finished!')
     # print(apiKey)
@@ -20,22 +24,29 @@ def getCollection(username, apiKey, collectionID):
     response = requests.get(url, {'apikey': apiKey})
     return response.json()
 
-def downloadWallpapers(collection):
+# Main function that loops through the collection and downloads each one
+# Wallhaven has a 45/min API rate limit, so this will wait 2 seconds (safer side) between downloads
+def downloadWallpapers(collection, collectionID):
+    # create directory 
+    downloadDirectory = os.path.join('downloads/', collectionID, '')
+    print(downloadDirectory)
+    os.makedirs(os.path.dirname(downloadDirectory), exist_ok=True)
+
+    # for tracking status of collection downloaded
     totalWallpapers = collection['meta']['total']
     current = 1
+
+    # main loop
     for wallpaper in collection['data']:
         time.sleep(2)
         print("downloading: %i/%i..." % (current, totalWallpapers))
-        download(wallpaper['path'])
+        download(wallpaper['path'], downloadDirectory)
         current += 1
 
         
 
 
-def download(url):
-    downloadDirectory = 'downloads/'
-    # create the download directory if it doesn't exist
-    os.makedirs(os.path.dirname(downloadDirectory), exist_ok=True)
+def download(url, downloadDirectory):
     # extract the filename from the URL
     filename = os.path.basename(url)
     # create the filepath to write to 
