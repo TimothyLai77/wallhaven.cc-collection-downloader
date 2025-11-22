46d3c56a6cfbaabdc04c590252b896c60affdf95
timothy <lai.timothy77@gmail.com>
2025-11-22 13:58:41 -0500

    checks for duplicates

--- downloader.py
+++ downloader.py
@@ -3,13 +3,17 @@ import requests
 import os
 import time
 
+# download path
 baseDownloadPath='downloads/'
 fullDownloadDirectory=''
+
 # for tracking progress
 totalWallpapers = None
 current = 1
 currentPage = 1
 
+# duplicate checking with sets
+existingWallpapers = set()
 
 # pass in API Key, and the collection you want to download
 def main():
@@ -17,13 +21,16 @@ def main():
     global fullDownloadDirectory
     global totalWallpapers
     global currentPage
+    global existingWallpapers
+
     username = sys.argv[1]
     apiKey = sys.argv[2]
     collectionID = sys.argv[3]
     
     # create the download path
     fullDownloadDirectory = os.path.join(baseDownloadPath, collectionID, '')
-    
+    existingWallpapers = populateSet(existingWallpapers, fullDownloadDirectory)
+
     # get metadata about collection from API
     collection = getCollection(username, apiKey, collectionID);    
 
@@ -43,6 +50,13 @@ def main():
 
     print('Finished!')
 
+
+# go into a directory and put all the files into a set for duplicate checking
+def populateSet(set, directory):
+    set.update(os.listdir(directory))
+    return set
+
+
 # Get the list of wallpapers (up to 24, wallhaven paginates 24 at a time)
 def getCollection(username, apiKey, collectionID, page=1):
     url = 'https://wallhaven.cc/api/v1/collections/%s/%s' % (username, collectionID)
@@ -58,14 +72,18 @@ def downloadWallpapers(collection, collectionID):
     os.makedirs(os.path.dirname(fullDownloadDirectory), exist_ok=True)
 
     # main loop, wallhaven paginates a set amount at a time. so exit when current page is done.
+    
     for wallpaper in collection['data']:
-        time.sleep(2)
-        print("downloading: %i/%i..." % (current, totalWallpapers))
-        download(wallpaper['path'], fullDownloadDirectory)
+        currentFile = os.path.basename(wallpaper['path'])
+        if(currentFile not in existingWallpapers):
+            time.sleep(2)
+            print("[%i/%i]: downloading..." % (current, totalWallpapers))
+            download(wallpaper['path'], fullDownloadDirectory)
+        else:
+            print('[%i/%i]: wallpaper %s already exists, skipping...'%(current, totalWallpapers, currentFile))
         current += 1
 
         
-
 
 def download(url, downloadDirectory):
     # extract the filename from the URL
